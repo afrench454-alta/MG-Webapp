5dff05fc0bec99513283b8b30deb23b8e55427fb
french a <ashtonfrench454@gmail.com>
2026-09-17 08:07:28 +1000

    fix: load desktop nav overrides from console entry page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,6 +2,7 @@ import { redirect } from "next/navigation";
 
 import { signOutAction } from "@/app/actions/auth";
 import { ConsoleApp } from "@/features/console/console-app";
+import "@/features/console/console-desktop-nav.css";
 import { archiveClientAction, commitClientImportAction, previewClientImportAction, saveClientAction } from "@/features/console/data/client-actions";
 import { listClients } from "@/features/console/data/client-repository";
 import { deleteJobRequestAction, saveJobRequestAction } from "@/features/console/data/job-request-actions";
